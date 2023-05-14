21940764aff56b371b332c1808b67438ac9c3ea4
sahil_sagwekar <sahilss2652@gmail.com>
2023-05-14 18:44:41 +0530

    bug fix index.js

--- static/js/index.js
+++ static/js/index.js
@@ -28,32 +28,32 @@ $fileInput.on('change', function() {
 
   
 
-const pdfInput = document.getElementById("pdf-input");
-const uploadButton = document.getElementById("upload-button");
-const output = document.getElementById("output");
+// const pdfInput = document.getElementById("pdf-input");
+// const uploadButton = document.getElementById("upload-button");
+// const output = document.getElementById("output");
 
-uploadButton.addEventListener("click", () => {
-  // Get the selected file
-  const file = pdfInput.files[0];
+// uploadButton.addEventListener("click", () => {
+//   // Get the selected file
+//   const file = pdfInput.files[0];
 
-  // Check if the file is a PDF file
-  if (!file.type.startsWith("application/pdf")) {
-    alert("Please select a PDF file.");
-    return;
-  }
+//   // Check if the file is a PDF file
+//   if (!file.type.startsWith("application/pdf")) {
+//     alert("Please select a PDF file.");
+//     return;
+//   }
 
-  // Get the file name
-  const fileName = file.name;
+//   // Get the file name
+//   const fileName = file.name;
 
-  // Check if the file name is correct
-  if (!fileName.endsWith(".pdf")) {
-    alert("Please select a valid PDF file.");
-    return;
-  }
+//   // Check if the file name is correct
+//   if (!fileName.endsWith(".pdf")) {
+//     alert("Please select a valid PDF file.");
+//     return;
+//   }
 
-  // Redirect to the new page
-  window.location.href = "chat.html";
-});
+//   // Redirect to the new page
+//   window.location.href = "chat.html";
+// });
 
 
 
